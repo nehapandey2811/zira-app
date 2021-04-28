f90b08aded865acc7375bec6f019ac4efb153d3d
Neha Pandey <nehapandey2811@gmail.com>
2021-04-28 11:37:00 +0530

    Local Storage

--- script.js
+++ script.js
@@ -2,92 +2,126 @@ let TC = document.querySelector(".ticket-container");
 let allFilters = document.querySelectorAll(".filter");
 let modalVisible = false;
 
-for(let i = 0 ; i < allFilters.length; i++) {
+let allTasks = localStorage.getItem("allTasks");
+if (allTasks != null) {
+    allTasks = JSON.parse(allTasks);
+    for (let i = 0; i < allTasks.length; i++) {
+        let ticket = document.createElement("div");
+        ticket.classList.add("ticket");
+        ticket.innerHTML = `<div class="ticket-color ticket-color-${allTasks[i].priority}"></div>
+                        <div class="ticket-id">Ticket ID: ${allTasks[i].ticketId}</div>
+                        <div class="task">${allTasks[i].task}</div>`;
+        TC.appendChild(ticket);
+        ticket.addEventListener("click", function (e) {
+            if (e.currentTarget.classList.contains("active")) {
+                e.currentTarget.classList.remove("active");
+            } else {
+                e.currentTarget.classList.add("active");
+            }
+        });
+    }
+}
+
+for (let i = 0; i < allFilters.length; i++) {
     allFilters[i].addEventListener("click", filterHandler);
 }
 
-function filterHandler (e) {
+function filterHandler(e) {
 
 }
 let addBtn = document.querySelector(".add");
+let deleteBtn = document.querySelector(".delete");
+
+deleteBtn.addEventListener("click", function (e) {
+    let selectedTickets = document.querySelectorAll(".ticket.active");
+    let allTasks = JSON.parse(localStorage.getItem("allTasks"));
+    for (let i = 0; i < selectedTickets.length; i++) {
+        selectedTickets[i].remove();
+        let ticketID = selectedTickets[i].querySelector(".ticket-id").innerText;
+        allTasks = allTasks.filter(function (data) {
+            return (("Ticket ID: " + data.ticketId) != ticketID);
+        });
+    }
+    localStorage.setItem("allTasks", JSON.stringify(allTasks));
+});
 
 addBtn.addEventListener("click", showModal);
 
 let selectedPriority;
 
 function showModal(e) {
-    if(!modalVisible) {
-        // let modal = document.createElement("div");
-        // modal.classList.add("modal");
-        // modal.innerHTML = `<div class="task-to-be-added" data-typed="false" contenteditable="true"></div>
-        // <div class="modal-priority-list">
-        //     <div class="modal-red-filter modal-filter"></div>
-        //     <div class="modal-blue-filter modal-filter"></div>
-        //     <div class="modal-green-filter  modal-filter"></div>
-        //     <div class="modal-yellow-filter  modal-filter"></div>
-        // </div>`;
-        // TC.appendChild(modal);
-
-        let modal = `<div class="modal">
-            <div class="task-to-be-added" data-typed="false" contenteditable="true">Enter your task here.</div>
+    if (!modalVisible) {
+        let modal = document.createElement("div");
+        modal.classList.add("modal");
+        modal.innerHTML = `<div class="task-to-be-added" data-typed="false" contenteditable="true">Enter your task here.</div>
             <div class="modal-priority-list">
                 <div class="modal-red-filter modal-filter active"></div>
                 <div class="modal-blue-filter modal-filter"></div>
                 <div class="modal-green-filter  modal-filter"></div>
                 <div class="modal-yellow-filter  modal-filter"></div>
-            </div>
-        </div>`;
-        TC.innerHTML = TC.innerHTML + modal;
+            </div>`;
+        TC.appendChild(modal);
         selectedPriority = "red"; //by default
         let taskModal = document.querySelector(".task-to-be-added");
-        taskModal.addEventListener("click", function(e) {
-            if(e.currentTarget.getAttribute("data-typed") == "false") {
+        taskModal.addEventListener("click", function (e) {
+            if (e.currentTarget.getAttribute("data-typed") == "false") {
                 e.currentTarget.innerText = "";
                 e.currentTarget.setAttribute("data-typed", "true");
             }
         })
         modalVisible = true;
-        taskModal.addEventListener("keypress", addTicket.bind(this,taskModal));
+        taskModal.addEventListener("keypress", addTicket.bind(this, taskModal));
         let modalFilters = document.querySelectorAll(".modal-filter");
-        for(let i = 0; i < modalFilters.length; i++) {
-            modalFilters[i].addEventListener("click", selectPriority);
+        for (let i = 0; i < modalFilters.length; i++) {
+            modalFilters[i].addEventListener("click", selectPriority.bind(this,taskModal));
         }
     }
 
 }
 
-function selectPriority(e) {
+function selectPriority(taskModal, e) {
     let activeFIlter = document.querySelector(".modal-filter.active");
     activeFIlter.classList.remove("active");
     selectedPriority = e.currentTarget.classList[0].split("-")[1];
     e.currentTarget.classList.add("active");
+    taskModal.click();
+    taskModal.focus();
 }
 
 
-function addTicket(taskModal,e) {
-    if(e.key == "Enter" && e.shiftKey == false && taskModal.innerText.trim() != "") {
+function addTicket(taskModal, e) {
+    if (e.key == "Enter" && e.shiftKey == false && taskModal.innerText.trim() != "") {
         let task = taskModal.innerText;
-
-        let ticket = `<div class="ticket">
-                        <div class="ticket-color ticket-color-${selectedPriority}"></div>
-                        <div class="ticket-id">#ashvhj</div>
-                        <div class="task">${task}</div>
-                    </div>`;
+        let id = uid();
+        let ticket = document.createElement("div");
+        ticket.classList.add("ticket");
+        ticket.innerHTML = `<div class="ticket-color ticket-color-${selectedPriority}"></div>
+                        <div class="ticket-id">Ticket ID: ${id}</div>
+                        <div class="task">${task}</div>`;
 
         document.querySelector(".modal").remove();
         modalVisible = false;
-        TC.innerHTML = TC.innerHTML + ticket;
-    } else if(e.key == "Enter" && e.shiftKey == false) {
+        TC.appendChild(ticket);
+        ticket.addEventListener("click", function (e) {
+            if (e.currentTarget.classList.contains("active")) {
+                e.currentTarget.classList.remove("active");
+            } else {
+                e.currentTarget.classList.add("active");
+            }
+        });
+
+        let allTasks = localStorage.getItem("allTasks");
+
+        if (allTasks == null) {
+            let data = [{ "ticketId": id, "task": task, "priority": selectedPriority }];
+            localStorage.setItem("allTasks", JSON.stringify(data));
+        } else {
+            let data = JSON.parse(allTasks);
+            data.push({ "ticketId": id, "task": task, "priority": selectedPriority });
+            localStorage.setItem("allTasks", JSON.stringify(data));
+        }
+    } else if (e.key == "Enter" && e.shiftKey == false) {
         e.preventDefault();
         alert("Error! You have not typed anything in task.")
     }
 }
-
-// for (let i = 0; i < allFilters.length; i++) {
-//     allFilters[i].addEventListener("click", filterHandler);
-// }
-
-// function filterHandler(e) {
-//     let filterColor = e.currentTarget.children[0].classList[0].split("-")[0];
-//     TC.style.backgroundColor = filterColor;
-// }
